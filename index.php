6e5f629dabbf31f19a8edcf59ec8b9a645ddf810
SivaNurI <sivanurislamii@gmail.com>
2019-09-24 20:44:15 +0700

    Bootstrap

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Latihan Bootstrap</title>
+	<!--Pemanggilan CSS-->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<!-- Akhir Pemanggilan CSS -->
+</head>
+<body>
+      <!-- Header -->
+      <!-- End Header -->
+
+      <!-- Content -->
+      <div class="container-fluid">
+      	 <div class="row">
+      		<div class="col-md-12" style="background:violet">
+      			<h1>12 Column</h1>
+      		</div>
+      	</div>
+      	<div class="row">
+      		<div class="col-md-6" style="background:red">
+      			<h1>6 Column</h1>
+      		</div>
+      		<div class="col-md-6" style="background:blue">
+      			<h1>6 Column</h1>
+      		</div>
+      	</div>
+      </div>
+      <!-- End Content -->
+
+      <!-- Footer -->
+      <!-- End Footer -->
+
+      <!--Pemanggilan JS -->
+      <script src="assets/js/bootstrap.bundle.js"></script>
+      <script src="assets/js/bootstrap.bundle.min.js"></script>
+      <!--Akhir Pemanggilan JS -->
+</body>
+</html>
